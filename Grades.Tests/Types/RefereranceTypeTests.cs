43d607661e78439c27f66bd7ea80cb9150e10c04
Nicoleta Andruchovici <nicoleta.andruchovici@gmail.com>
2017-07-10 17:02:32 +0200

    Grade Book first version

---
+++ Grades.Tests/Types/RefereranceTypeTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grades.Tests.Types
+{
+    [TestClass]
+    public class RefereranceTypeTests
+    {
+        [TestMethod]
+        public void IntVariablesHoldAValue()              
+        {
+            int x1 = 100;
+            int x2 = x1;
+
+            x1 = 4;
+            Assert.AreNotEqual(x1, x2);
+        }
+
+        [TestMethod]
+        public void GradeBookVariablesHoldAReferance()
+        {
+            GradeBook g1 = new GradeBook();
+            GradeBook g2 = g1;
+
+            g1 = new GradeBook();
+            g1.Name = "Nicoleta's grade book";
+            Assert.AreNotEqual(g1.Name, g2.Name);
+        }
+    }
+}
